86271faa3982ba7b04bd1b6bbe9b1d490c1ede54
Andjaradji <andjaradji.rooseno@klikacc.com>
2020-10-10 10:34:28 +0700

    adding hell_python.py files

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionMixin
+
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionMixin):
+    """ Represent "UserProfile" inside our system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
