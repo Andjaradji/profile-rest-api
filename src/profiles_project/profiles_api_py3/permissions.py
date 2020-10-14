b3932306e121570fea8b18a7288d4819d1adb44a
Andjaradji <andjaradji.rooseno@klikacc.com>
2020-10-14 19:22:40 +0700

    Added permissions and authentication

---
+++ src/profiles_project/profiles_api_py3/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """ Allow User to edit their own profile """
+
+    def has_object_permission(self, request,view, obj):
+        """ Check User who is trying edit their own profie. """
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.id == request.user.id
+        
